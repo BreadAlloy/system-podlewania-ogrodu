2e9850ef30cb7957e1a281c20f6b1e9d6edc0343
Krzysztof <kupczykkrzys@gmail.com>
2025-11-16 20:54:28 +0100

    Mały fix kolejności w rozpisce_sekcji

--- konfiguracja.py
+++ konfiguracja.py
@@ -19,19 +19,19 @@ class konfiguracja:
     # w normalnym uzytkowaniu nie będzie się używać "dodatkowa n" tylko po prostu się nie wpisze
     rozpiska_sekcji = {
 # id_sekcji  :  (nazwa, numer_gpio)
-        0 : ("dodatkowa 1", 10),
-        1 : ("dodatkowa 2", 24),
-        2 : ("dodatkowa 3", 9),
-        3 : ("Nowe drzewa", 25),
-        4 : ("Rabatki kolo domu", 11),
-        5 : ("Rabatka gorka", 23),
-        6 : ("Trawnik kolo domu: kasztan, kuchnia", 5),
-        7 : ("Trawnik kolo domu: przed domem", 18),
-        8 : ("Trawnik kolo domu: judaszowniec, magnolia", 6),
-        9 : ("Trawnik daleko od domu: kolo szopki", 12),
-       10 : ("Trawnik daleko od domu: dołek", 13),
-       11 : ("Trawnik daleko od domu: las judaszowcowy", 16),
-       12 : ("Trawnik daleko od domu: duże drzewa przy drodze", 20),
+        0 : ("Nowe drzewa", 25),
+        1 : ("Rabatki kolo domu", 11),
+        2 : ("Rabatka gorka", 23),
+        3 : ("Trawnik kolo domu: kasztan, kuchnia", 5),
+        4 : ("Trawnik kolo domu: przed domem", 18),
+        5 : ("Trawnik kolo domu: judaszowniec, magnolia", 6),
+        6 : ("Trawnik daleko od domu: kolo szopki", 12),
+        7 : ("Trawnik daleko od domu: dołek", 13),
+        8 : ("Trawnik daleko od domu: las judaszowcowy", 16),
+        9 : ("Trawnik daleko od domu: duże drzewa przy drodze", 20),
+       10 : ("dodatkowa 1", 10),
+       11 : ("dodatkowa 2", 24),
+       12 : ("dodatkowa 3", 9),
        13 : ("dodatkowa 4", 19),
        14 : ("dodatkowa 5", 21)
     };
