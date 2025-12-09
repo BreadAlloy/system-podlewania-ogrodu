7335aa4c56621f2f1b600338e4efd0199c15e071
Goat2137 <dawcel78@op.pl>
2025-12-09 16:05:26 +0100

    dodanie logowania i eksport planow. W planach dodanie tez importu

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
-from apps.SPO.views import ZaworyView, ZaworCreateView, ZaworONOFFView, WodomierzView
+from apps.SPO.views import ZaworyView, ZaworCreateView, ZaworONOFFView, WodomierzView, LogiView
 
 urlpatterns = [
     path("", ZaworyView.as_view(), name="landing"),
-    path("zawory/",ZaworyView.as_view(), name="zawory"),
-    path("zawory/create",ZaworCreateView.as_view(), name="zawor_create"),
-    path("zawory/<int:zawor_id>/",ZaworONOFFView, name="switch"),
-    path("wodomierz/",WodomierzView.as_view(), name="wodomierz")
+    path("zawory/", ZaworyView.as_view(), name="zawory"),
+    path("zawory/create", ZaworCreateView.as_view(), name="zawor_create"),
+    path("zawory/<int:zawor_id>/", ZaworONOFFView, name="switch"),
+    path("wodomierz/", WodomierzView.as_view(), name="wodomierz"),
+    path('logi/', LogiView.as_view(), name='logi')
 ]
