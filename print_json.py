b42610cbe399fd34f97612ba35eebe09c8337bd9
Kadzetan <117111343+Kadzetan@users.noreply.github.com>
2025-12-11 08:30:36 +0100

    print_json.py
    Teraz jest git

--- print_json.py
+++ print_json.py
@@ -1,7 +1,7 @@
 import json
 import pprint
 
-file_path = 'plany_export.json'
+file_path = 'system-podlewania-ogrodu/plany_export.json'
 
 with open(file_path, 'r', encoding='utf-8') as file:
     data = json.load(file)
