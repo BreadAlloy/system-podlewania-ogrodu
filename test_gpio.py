9ce3cf3e034924526787e452b2ad4f6d07371b8f
Filip Kojro <filip@c68-225.wmi.amu.edu.pl>
2025-12-17 12:48:06 +0100

    opis tworzenia testow + requirements for tests

---
+++ test_gpio.py
@@ -0,0 +1,19 @@
+import pytest
+
+# w tym pliku prosze dopisac nastepne testy
+
+# testy jakis modeli,... w django jezeli beda potrzebne bedziemy robic w django, narazie zostanmy przy prostych
+
+# w pliku requirments_testy.txt prosze dodac biblioteki wymagane do uruchomienia testow
+
+
+# struktura testu (po wiecej informacji zapraszam do dokumentacji pytest)
+# def nazwa_testu():
+#   przygotowanie danych do testu (nie zawsze potrzebne)
+#   assert (juz konkretny test)
+
+# ponizej przyklad
+
+def test_example():
+    print("hello")
+    assert True == True
