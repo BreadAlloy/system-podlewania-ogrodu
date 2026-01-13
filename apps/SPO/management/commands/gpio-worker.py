26b4cbeed6899aaa4b8782defe54a78f97d755da
Krzysztof <kupczykkrzys@gmail.com>
2026-01-13 14:24:17 +0100

    Naprawiłem swoje.
    Jeszcze dane defaultowe do ProgramCreateView by się przydały.

--- apps/SPO/management/commands/gpio-worker.py
+++ apps/SPO/management/commands/gpio-worker.py
@@ -64,12 +64,10 @@ class Command(BaseCommand):
                     self.plan.usun_program(odebrane[1]);
 
                 elif(odebrane[0].kod == kody_komunikatow.ZMODYFIKUJ_PROGRAM):
-                    print(odebrane[1])
-                    self.plan.zmodyfikuj_program(odebrane[1].nazwa_programu,odebrane[1]);
+                    self.plan.zmodyfikuj_program(odebrane[1]);
 
                 elif(odebrane[0].kod == kody_komunikatow.DODAJ_PROGRAM):
-                    print(odebrane[1])
-                    self.plan.dodaj_program(odebrane[1].nazwa_programu,odebrane[1]);
+                    self.plan.dodaj_program(odebrane[1]);
 
                 else:
                     print("Cos tu nie tak");
